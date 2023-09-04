1507f403e13d1336c6c592ebcdc513b075a8afaf
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-04 04:47:32 +0000

    Enhancement (ci): Bump PSModulePublisher to `v0.6.0`

--- build/definitions/modulemanifest.ps1
+++ build/definitions/modulemanifest.ps1
@@ -23,7 +23,7 @@
     # FormatsToProcess = @()
     # NestedModules = @()
     FunctionsToExport = @(
-        Get-ChildItem $PSScriptRoot/../../../src/Log-Rotate/public -Exclude *.Tests.ps1 | % { $_.BaseName }
+        Get-ChildItem $PSScriptRoot/../../src/Log-Rotate/public -Exclude *.Tests.ps1 | % { $_.BaseName }
     )
     CmdletsToExport = @()
     VariablesToExport = @()
