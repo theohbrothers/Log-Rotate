7e424e0f6f5c638c295344f91da3460a3f46ed49
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-10-17 16:04:50 +0000

    Refactor (tests): Make tests compatible with Pester v5

--- src/Log-Rotate/Log-Rotate.Integration.Tests.ps1
+++ src/Log-Rotate/Log-Rotate.Integration.Tests.ps1
@@ -1,82 +1,92 @@
 Describe 'Log-Rotate' -Tag 'Integration' {
 
-    $drive = Convert-Path 'TestDrive:\'
+    BeforeEach {
+        $drive = Convert-Path 'TestDrive:\'
 
-    $logDir = Join-Path $drive 'logs'
-    $logOldDir = Join-Path $drive 'oldlogs'
-    $logFile = Join-Path $logDir 'foo.log'
-    $logFileContent = 'foo-bar'
+        $logDir = Join-Path $drive 'logs'
+        $logOldDir = Join-Path $drive 'oldlogs'
+        $logFile = Join-Path $logDir 'foo.log'
+        $logFileContent = 'foo-bar'
 
-    $logDir2 = Join-Path $drive 'logs2'
-    $logOldDir2 = Join-Path $drive 'oldlogs2'
-    $logFile2 = Join-Path $logDir2 'foo.log'
-    $logFile2Content = 'foo-bar'
+        $logDir2 = Join-Path $drive 'logs2'
+        $logOldDir2 = Join-Path $drive 'oldlogs2'
+        $logFile2 = Join-Path $logDir2 'foo.log'
+        $logFile2Content = 'foo-bar'
 
-    $configDir = Join-Path $drive 'config'
-    $configFile = Join-Path $configDir 'logrotate.conf'
-    $configFileContent = @"
+        $configDir = Join-Path $drive 'config'
+        $configFile = Join-Path $configDir 'logrotate.conf'
+        $configFileContent = @"
 "$logFile" {
     rotate 3
 }
 "@
 
-    $configDir2 = Join-Path $drive 'config2'
-    $configFile2 = Join-Path $configDir2 'logrotate2.conf'
-    $configFile2Content = @"
+        $configDir2 = Join-Path $drive 'config2'
+        $configFile2 = Join-Path $configDir2 'logrotate2.conf'
+        $configFile2Content = @"
 "$logFile" {
     rotate 3
 }
 "@
 
-    $stateDir = Join-Path $drive 'state'
-    $stateFile = Join-Path $stateDir 'Log-Rotate.status'
+        $stateDir = Join-Path $drive 'state'
+        $stateFile = Join-Path $stateDir 'Log-Rotate.status'
 
-    function Init  {
-        New-Item $configDir -ItemType Directory -Force > $null
-        New-Item $configFile -ItemType File -Force > $null
-        # Do not write an empty file with BOM in Powershell <= 5
-        if ($configFileContent) {
-            $configFileContent | Out-File $configFile -Encoding utf8 -Force -NoNewline
+        $eaPreference = 'Continue'
+
+        function Init  {
+            New-Item $configDir -ItemType Directory -Force > $null
+            New-Item $configFile -ItemType File -Force > $null
+            # Do not write an empty file with BOM in Powershell <= 5
+            if ($configFileContent) {
+                $configFileContent | Out-File $configFile -Encoding utf8 -Force -NoNewline
+            }
+
+            New-Item $configDir2 -ItemType Directory -Force > $null
+            New-Item $configFile2 -ItemType File -Force > $null
+            # Do not write an empty file with BOM in Powershell <= 5
+            if ($configFile2Content) {
+                $configFile2Content | Out-File $configFile2 -Encoding utf8 -Force -NoNewline
+            }
+
+            New-Item $logDir -ItemType Directory -Force > $null
+            New-Item $logOldDir -ItemType Directory -Force > $null
+            New-Item $logFile -ItemType File -Force > $null
+            # Do not write an empty file with BOM in Powershell <= 5
+            if ($logFileContent) {
+                $logFileContent | Out-File $logFile -Encoding utf8 -Force -NoNewline
+            }
+
+            New-Item $logDir2 -ItemType Directory -Force > $null
+            New-Item $logOldDir2 -ItemType Directory -Force > $null
+            New-Item $logFile2 -ItemType File -Force > $null
+            if ($logFile2Content) {
+                $logFile2Content | Out-File $logFile2 -Encoding utf8 -Force -NoNewline
+            }
+
+            New-Item $stateDir -ItemType Directory -Force > $null
         }
 
-        New-Item $configDir2 -ItemType Directory -Force > $null
-        New-Item $configFile2 -ItemType File -Force > $null
-        # Do not write an empty file with BOM in Powershell <= 5
-        if ($configFile2Content) {
-            $configFile2Content | Out-File $configFile2 -Encoding utf8 -Force -NoNewline
+        function Cleanup  {
+            Get-Item $logDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+            Get-Item $logDir2 -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+            Get-Item $logOldDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+            Get-Item $logOldDir2 -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+            Get-Item $configDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+            Get-Item $configDir2 -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+            Get-Item $stateDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
         }
-
-        New-Item $logDir -ItemType Directory -Force > $null
-        New-Item $logOldDir -ItemType Directory -Force > $null
-        New-Item $logFile -ItemType File -Force > $null
-        # Do not write an empty file with BOM in Powershell <= 5
-        if ($logFileContent) {
-            $logFileContent | Out-File $logFile -Encoding utf8 -Force -NoNewline
-        }
-
-        New-Item $logDir2 -ItemType Directory -Force > $null
-        New-Item $logOldDir2 -ItemType Directory -Force > $null
-        New-Item $logFile2 -ItemType File -Force > $null
-        if ($logFile2Content) {
-            $logFile2Content | Out-File $logFile2 -Encoding utf8 -Force -NoNewline
-        }
-
-        New-Item $stateDir -ItemType Directory -Force > $null
     }
 
-    function Cleanup  {
-        Get-Item $logDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
-        Get-Item $logDir2 -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
-        Get-Item $logOldDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
-        Get-Item $logOldDir2 -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
-        Get-Item $configDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
-        Get-Item $configDir2 -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
-        Get-Item $stateDir -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
+    AfterEach {
+        Cleanup
     }
 
     Context 'Behavior from flags' {
 
-        $eaPreference = 'Stop'
+        BeforeEach {
+            $eaPreference = 'Stop'
+        }
 
         It 'rotates a log file' {
             Init
@@ -93,8 +103,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It 'rotates a log file when forced' {
@@ -124,8 +132,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
             # Assert that the oldest rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).2"
-
-            Cleanup
         }
 
         It 'does not rotate a log file in debug mode' {
@@ -137,8 +143,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
             # Assert that the log file should not be gone
             $logItem = Get-Item $logFile -ErrorAction SilentlyContinue
             $logItem | Should -Not -Be $null
-
-            Cleanup
         }
 
         It 'does not rotate a log file when forced in debug mode ' {
@@ -151,14 +155,10 @@ Describe 'Log-Rotate' -Tag 'Integration' {
             # Assert that the log file should not be gone
             $logItem = Get-Item $logFile -ErrorAction SilentlyContinue
             $logItem | Should -Not -Be $null
-
-            Cleanup
         }
     }
 
     Context 'Behavior from configuration options' {
-
-        $eaPreference = 'Continue'
 
         It "Option 'compress': rotates a log file and compresses it" {
             $configFileContent = @"
@@ -184,8 +184,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1.gz"
-
-            Cleanup
         }
 
         It "Option 'compress' with 'compressoptions': rotates a log file and compresses it" {
@@ -212,8 +210,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1.gz"
-
-            Cleanup
         }
 
         It "Option 'compress' with 'compressext': rotates a log file and compresses it, renaming to a custom extension" {
@@ -240,8 +236,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1.foo"
-
-            Cleanup
         }
 
         It "Option 'copy': rotates a log file as a copy" {
@@ -271,8 +265,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
             $logFileHash = Get-FileHash $logFile -Algorithm md5
             $rotatedLogFileHash = Get-FileHash $rotatedLogItems[0].FullName -Algorithm md5
             $logFileHash.Hash | Should -Be $rotatedLogFileHash.Hash
-
-            Cleanup
         }
 
         It "Option 'copytruncate': rotates a log file as a copy and truncates the original" {
@@ -303,8 +295,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
             $logFileHash = Get-FileHash $logFile -Algorithm md5
             $rotatedLogFileHash = Get-FileHash $rotatedLogItems[1].FullName -Algorithm md5
             $logFileHash.Hash | Should -Not -Be $rotatedLogFileHash.Hash
-
-            Cleanup
         }
 
         It "Option 'create': rotates a log file and immediately creates a new original file" {
@@ -330,8 +320,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'daily': rotates a log file only once daily" {
@@ -366,8 +354,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'dateext': rotates a log file with a date extension" {
@@ -391,8 +377,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf )$( Get-Date -UFormat '-%Y%m%d' )"
-
-            Cleanup
         }
 
         It "Option 'dateext' with 'dateformat': rotates a log file with a date extension with a custom date format" {
@@ -417,8 +401,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf )$( Get-Date -UFormat '-%Y%m%d' )"
-
-            Cleanup
         }
 
         It "Option 'delaycompress': rotates a log file, but delays compressing the newest rotated file" {
@@ -468,8 +450,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
             # Assert that the oldest rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).2.gz"
-
-            Cleanup
         }
 
         It "Option 'firstaction': rotates a log file with a firstaction script" {
@@ -498,8 +478,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Expect that the script was run
             $result | Should -Be 'foo'
-
-            Cleanup
         }
 
         It "Option 'ifempty': rotates a log file even if it is empty" {
@@ -525,8 +503,6 @@ Describe 'Log-Rotate' -Tag 'Integration' {
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'include': rotates a log file, when 'include' refers to a directory" {
@@ -553,8 +529,6 @@ include $configDir2
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'include': rotates a log file, when 'include' refers to a file" {
@@ -581,8 +555,6 @@ include $configFile2
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'lastaction': rotates a log file with a lastaction script" {
@@ -611,8 +583,6 @@ include $configFile2
 
             # Expect that the script was run
             $result | Should -Be 'foo'
-
-            Cleanup
         }
 
         It "Option 'missingok': rotates a log file even when other patterns don't match any log files" {
@@ -637,8 +607,6 @@ include $configFile2
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'monthly': rotates a log file only once monthly" {
@@ -673,8 +641,6 @@ include $configFile2
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'nocopy': rotate a log file, but not as a copy" {
@@ -696,8 +662,6 @@ copy
             $rotatedLogItems = @( Get-Item $logDir/* )
             $rotatedLogItems.Count | Should -Be 1
             $rotatedLogItems[0] | Should -BeOfType [System.IO.FileSystemInfo]
-
-            Cleanup
         }
 
         It "Option 'nocopytruncate': rotate a log file, but not as a copy to be truncated" {
@@ -722,8 +686,6 @@ copytruncate
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'nocreate': rotate a log file, but do not create a new a log file" {
@@ -745,8 +707,6 @@ create
             $rotatedLogItems = @( Get-Item $logDir/* )
             $rotatedLogItems.Count | Should -Be 1
             $rotatedLogItems[0] | Should -BeOfType [System.IO.FileSystemInfo]
-
-            Cleanup
         }
 
         It "Option 'nodateext': rotates a log file without a date extension" {
@@ -771,8 +731,6 @@ dateext
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'nodelaycompress': rotates a log file, but does not delay compressing the newest rotated file" {
@@ -823,8 +781,6 @@ delaycompress
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1.gz"
             # Assert that the oldest rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).2.gz"
-
-            Cleanup
         }
 
         It "Option 'nomissingok': rotate files, while issuing an error (warning) for pattern that don't match any log files" {
@@ -850,8 +806,6 @@ missingok
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'noolddir': rotates a log file, but not into an olddir" {
@@ -876,8 +830,6 @@ olddir $logOldDir
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'nosharedscripts': rotates two log files, running a shared script only once" {
@@ -925,8 +877,6 @@ sharedscripts
 
             # Assert that the rotated log file should be named
             $rotatedLogItems2[0].Name | Should -Be "$( Split-Path $logFile2 -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'notifempty': do not rotates a log file if it is empty" {
@@ -950,8 +900,6 @@ ifempty
             $rotatedLogItems.Count | Should -Be 1
             $rotatedLogItems[0] | Should -BeOfType [System.IO.FileSystemInfo]
             $rotatedLogItems[0].Length | Should -Be 0
-
-            Cleanup
         }
 
         It "Option 'olddir': rotates a log file into an olddir" {
@@ -975,8 +923,6 @@ ifempty
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'postrotate': rotates a log file with a postrotate script" {
@@ -1005,8 +951,6 @@ ifempty
 
             # Expect that the script was run
             $result | Should -Be 'foo'
-
-            Cleanup
         }
 
         It "Option 'preremove': rotates a log file with a preremove script" {
@@ -1043,8 +987,6 @@ ifempty
 
             # Expect that the script was run
             $result | Should -Be 'foo'
-
-            Cleanup
         }
 
         It "Option 'prerotate': rotates a log file with a prerotate script" {
@@ -1073,8 +1015,6 @@ ifempty
 
             # Expect that the script was run
             $result | Should -Be 'foo'
-
-            Cleanup
         }
 
         It "Option 'rotate': rotates a log file, keeping only a certain number of old files" {
@@ -1122,8 +1062,6 @@ ifempty
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
             # Assert that the oldest rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).2"
-
-            Cleanup
         }
 
         It "Option 'sharedscripts': rotates two log files, running a shared script only once" {
@@ -1168,8 +1106,6 @@ ifempty
 
             # Assert that the rotated log file should be named
             $rotatedLogItems2[0].Name | Should -Be "$( Split-Path $logFile2 -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'size': rotates a log file larger than specified by 'size'" {
@@ -1193,8 +1129,6 @@ ifempty
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'start': rotates a log file, with an numbered extension" {
@@ -1218,8 +1152,6 @@ ifempty
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[0].Name | Should -Be "$( Split-Path $logFile -Leaf ).100"
-
-            Cleanup
         }
 
         It "Option 'weekly': rotates a log file only once weekly" {
@@ -1254,8 +1186,6 @@ ifempty
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
 
         It "Option 'yearly': rotates a log file only once yearly" {
@@ -1290,8 +1220,6 @@ ifempty
 
             # Assert that the rotated log file should be named
             $rotatedLogItems[1].Name | Should -Be "$( Split-Path $logFile -Leaf ).1"
-
-            Cleanup
         }
     }
 }
